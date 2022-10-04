be23e501585589fb8323911cedea3feccb3beef9
IlyaVatlin <ilya.vatlin@gmail.com>
2022-10-04 10:45:40 +0300

    dz

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,28 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldCorrectAmount() {
+        assertEquals(service.remain(2800), 200);
+
+    }
+
+    @Test
+    public void shouldAmountZero() {
+        assertEquals(service.remain(0), 1000);
+    }
+
+    @Test
+    public void shouldBuyExtraIfTheAmount1000() {
+        assertEquals(service.remain(1000), 0);
+    }
+
+}
